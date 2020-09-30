5908add83932bdaa9c335143e931884e05f829a7
Elías Rodríguez Martín <elias@eliasrm.es>
2020-09-30 22:54:02 +0100

    First code

---
+++ freeRTOS10xx/MemMang/heap_5.d
@@ -0,0 +1,7 @@
+heap_5.o heap_5.d : ../freeRTOS10xx/MemMang/heap_5.c \
+ ../freeRTOS10xx/include/FreeRTOS.h \
+ ../freeRTOS10xx/include/FreeRTOSConfig.h \
+ ../freeRTOS10xx/include/projdefs.h ../freeRTOS10xx/include/portable.h \
+ ../freeRTOS10xx/include/portmacro.h \
+ ../freeRTOS10xx/include/mpu_wrappers.h ../freeRTOS10xx/include/task.h \
+ ../freeRTOS10xx/include/list.h
